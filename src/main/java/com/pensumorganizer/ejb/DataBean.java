34d61b0e58cae7bc68ff69e76a465824c947ca82
Mayers <ccmayers30@gmail.com>
2017-02-28 19:36:08 -0400

    Merge branch 'master' of https://github.com/emayers/PensumOrganizer
    # Conflicts:
    #	src/main/webapp/Vistas/Style/main.css
    #	src/main/webapp/Vistas/VistaLogin.xhtml
    #	src/main/webapp/Vistas/VistaPOAuto.xhtml

---
+++ src/main/java/com/pensumorganizer/ejb/DataBean.java
@@ -0,0 +1,24 @@
+package com.pensumorganizer.ejb;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.pensumorganizer.controller.GetProgram;
+import com.pensumorganizer.dao.Course;
+
+@ManagedBean
+@SessionScoped
+public class DataBean {
+	Map<Integer, List<Course>> trimester = GetProgram.getData();
+	
+	public Map<Integer, List<Course>> getTrimester() {
+		Map<Integer, List<Course>> newInstance = 
+				new LinkedHashMap<Integer, List<Course>>(trimester);
+		
+		return newInstance;
+	}
+}
---
+++ src/main/java/com/pensumorganizer/ejb/StudentBean.java
@@ -0,0 +1,21 @@
+package com.pensumorganizer.ejb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.pensumorganizer.dao.Student;
+
+@ManagedBean
+@SessionScoped
+public class StudentBean {
+
+	private Student student = new Student("Juan", "Vasquez", 1037777, "AGO - OCT", 2012, "IDS", 2010);
+
+	public Student getStudent() {
+		return student;
+	}
+	
+	public StudentBean() {
+	}
+	
+}
