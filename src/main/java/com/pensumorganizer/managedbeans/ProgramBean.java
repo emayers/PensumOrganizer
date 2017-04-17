3afbd7e2acfa97904138cb93294ddbccd2e3ff3b
Mayers <ccmayers30@gmail.com>
2017-04-17 19:43:29 -0400

    SUPER FIREFIGHTER

--- src/main/java/com/pensumorganizer/managedbeans/ProgramBean.java
+++ src/main/java/com/pensumorganizer/managedbeans/ProgramBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -19,6 +20,11 @@ public class ProgramBean implements Serializable {
 	
 	@EJB
 	ProgramEJBImpl pEJB = new ProgramEJBImpl();
+	
+	@PostConstruct
+	private void prepare(){
+		pEJB.recreateProgram();
+	}
 	
 	public Map<Integer, List<Course>> getProgram() {		
 		return pEJB.getProgram();
--- src/test/java/com/pensumorganizer/controller/Main.java
+++ src/test/java/com/pensumorganizer/controller/Main.java
@@ -1,23 +1,18 @@
 package com.pensumorganizer.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import com.pensumorganizer.dao.Course;
-import com.pensumorganizer.ejb.AutoPrioritizerEJBImpl;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Map<Integer, List<Course>> trimesters = AutoPrioritizerEJBImpl.getOrganizedPensum();
-		
-		for (int index = 0; index < trimesters.size(); index++) {
-			List<Course> current = trimesters.get(index);
-			System.out.println("Trimestre " + (index+1));
-			for (Course course : current) {
-				System.out.println(course.getId());
-			}
-		}
+//		Map<Integer, List<Course>> trimesters = getProgram();
+//		
+//		for (int index = 0; index < trimesters.size(); index++) {
+//			List<Course> current = trimesters.get(index);
+//			System.out.println("Trimestre " + (index+1));
+//			for (Course course : current) {
+//				System.out.println(course.getId());
+//			}
+//		}
 		
 //		Map<Integer, List<Course>> trimestersTaken = History.getHistory();
 //		
