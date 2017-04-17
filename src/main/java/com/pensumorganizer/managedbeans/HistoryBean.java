5d1c759561834c9b9254e8d43e5733c4249864e0
LAPTOP-KFP1F0JB\Ney Casilla\ZyenxD <neyc30@gmail.com>
2017-04-16 20:14:23 -0400

    History bean y otos arreglos

--- src/main/java/com/pensumorganizer/managedbeans/HistoryBean.java
+++ src/main/java/com/pensumorganizer/managedbeans/HistoryBean.java
@@ -3,24 +3,21 @@ package com.pensumorganizer.managedbeans;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import com.pensumorganizer.dao.Course;
-import com.pensumorganizer.ejb.GetHistory;
+import com.pensumorganizer.ejb.HistoryEJBImpl;
 
 @ManagedBean
 @SessionScoped
 public class HistoryBean {
-	Map<Integer, List<Course>> historic;
 	
-	@PostConstruct
-	public void prepare(){
-		historic = GetHistory.getHistory();
-	}
+	@EJB
+	HistoryEJBImpl hEJB = new HistoryEJBImpl();
 	
 	public Map<Integer, List<Course>> getTrimester() {
-		return historic;
+		return hEJB.getHistory();
 	}
 }
