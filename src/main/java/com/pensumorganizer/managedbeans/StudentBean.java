e785ab242a0416bccae10ef306da54f27fd7d21c
Mayers <ccmayers30@gmail.com>
2017-04-18 20:47:08 -0400

    meh

--- src/main/java/com/pensumorganizer/managedbeans/StudentBean.java
+++ src/main/java/com/pensumorganizer/managedbeans/StudentBean.java
@@ -1,19 +1,24 @@
 package com.pensumorganizer.managedbeans;
 
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import com.pensumorganizer.ejb.StudentEJBImpl;
+import com.pensumorganizer.dao.Student;
 
 @ManagedBean
 @SessionScoped
 public class StudentBean {
 
-	@EJB
-	StudentEJBImpl sEJB = new StudentEJBImpl();
+//	@EJB
+//	StudentEJBImpl sEJB = new StudentEJBImpl();
+//	
+//	public String getStudentName() {
+//		return sEJB.getStudentName();
+//	}
 	
-	public String getStudentName() {
-		return sEJB.getStudentName();
+	Student student = new Student("Sara", "Fermin", 1053691, "IDS", 2013, "IDS", 2010);
+	
+	public Student getStudent(){
+		return student;
 	}
 }
